de23fe5e9445d1497f100a1a92d7f03cf274b952
Paulius Gudonis <pg@neqsoft.com>
2020-01-11 10:24:00 +0100

    Added logo

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import Logo from '../../svg/Logo';
 
 const Header = () => (
   <header className="sticky top-0 bg-white shadow">
     <div className="container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8">
       <div className="flex items-center text-2xl">
+        <div className="w-12 mr-3">
+          <Logo />
+        </div>
         neqsoft
       </div>
       <div className="flex mt-4 sm:mt-0">
---
+++ src/svg/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Logo = () => (
+<svg width="100%" height="100%" viewBox="0 0 350 351" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="175" cy="174.377" r="21" fill="#4F4F4F"/>
+<ellipse rx="153" ry="35" transform="matrix(-0.817687 -0.575663 -0.575663 0.817687 175 175.377)" stroke="#484848" stroke-width="2" stroke-linecap="round"/>
+<ellipse rx="153" ry="35" transform="matrix(-0.817687 -0.575663 -0.575663 0.817687 175 175.377)" stroke="#484848" stroke-width="2" stroke-linecap="round"/>
+<circle r="11.5" transform="matrix(-0.817687 -0.575663 -0.575663 0.817687 100.46 160.955)" fill="#4F4F4F"/>
+<ellipse rx="153" ry="35" transform="matrix(0.813839 -0.581091 -0.581091 -0.813839 178.021 176.591)" stroke="#484848" stroke-width="2" stroke-linecap="round"/>
+<ellipse rx="153" ry="35" transform="matrix(0.813839 -0.581091 -0.581091 -0.813839 178.021 176.591)" stroke="#484848" stroke-width="2" stroke-linecap="round"/>
+<circle r="11.5" transform="matrix(0.813839 -0.581091 -0.581091 -0.813839 216.298 111.024)" fill="#4F4F4F"/>
+<ellipse cx="175" cy="175.377" rx="153" ry="35" stroke="#484848" stroke-width="2" stroke-linecap="round"/>
+<ellipse cx="175" cy="175.377" rx="153" ry="35" stroke="#484848" stroke-width="2" stroke-linecap="round"/>
+<circle cx="304.5" cy="193.877" r="11.5" fill="#4F4F4F"/>
+</svg>
+
+);
+
+export default Logo;
